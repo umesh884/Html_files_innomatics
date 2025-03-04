baa742afc1d960e1622e48bc3b46e65267ca6b87
umesh884 <58028497+umesh884@users.noreply.github.com>
2025-03-04 20:53:48 +0530

    Delete js-problem solving/onlineDiscountProducts.js

--- js-problem solving/onlineDiscountProducts.js
+++
@@ -1,9 +0,0 @@
-const products = [
-    {id:1,name1:"pizza", price:15.99},
-    {id:2,name1:"Burger", price:13.87},
-    {id:3,name1:"Shampoo", price:23.39},
-    {id:4,name1:"Detergent", price:40.40},
-    {id:5,name1:"milk", price:16.50},
-    {id:6,name1:"rice", price:100.20},
-    {id:7,name1:"sugar", price:20.35},
-    {id:8,name1:"oil", price:35.23}]
